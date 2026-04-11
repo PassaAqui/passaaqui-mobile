921e977e8e361c73fcea1d948f14abcabaae4b52
williamdev20 <williamalvs45@gmail.com>
2026-04-11 16:52:02 -0300

    refactor: remover login com o Google na tela Welcome

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -13,32 +13,27 @@ export default function Index() {
             <Image className="w-40 h-40" source={require("../../assets/logo/logo.png")} />
           </View>
 
-          <View className="justify-center mb-14 gap-3">
+          <View className="justify-center mb-16 gap-3 w-full">
             <Text className="text-white text-6xl text-center font-irishGrover">Recife GO</Text>
-            <Text className="text-white text-center font-irishGrover">Transforme a cidade em uma aventura</Text>
+            <Text className="text-white text-center font-irishGrover self-center max-w-56">Transforme a cidade em uma aventura</Text>
           </View>
 
           <View className="w-full gap-6">
             <View className="gap-6">
-              <Pressable className="bg-white p-4 rounded-2xl items-center">
-                <Text className="font-itim text-lg">Cadastrar</Text>
+              <Pressable className="border-[#EAAA6A] border-2 p-3 rounded-3xl items-center active:opacity-50">
+                <Text className="font-itim text-lg text-white">Continuar como aventureiro</Text>
               </Pressable>
+      
+              <View className="flex-row justify-center items-center gap-3">
+                <View className="w-28 h-px bg-white" />
+                <Text className="text-white font-itim">Ou</Text>
+                <View className="w-28 h-px bg-white" />
+              </View>
 
-              <Pressable className="bg-[#EAAA6A] p-4 rounded-2xl items-center">
-                <Text className="font-itim text-lg">Entrar</Text>
+              <Pressable className="bg-white p-3 rounded-3xl items-center flex-row gap-2 justify-center active:opacity-70">
+                <Text className="font-itim text-lg">Contiuar como comerciante</Text>
               </Pressable>
             </View>
-
-            <View className="flex-row justify-center items-center gap-3">
-              <View className="w-32 h-px bg-white" />
-              <Text className="text-white font-itim">Ou</Text>
-              <View className="w-32 h-px bg-white" />
-            </View>
-
-            <Pressable className="bg-white p-4 rounded-2xl items-center flex-row gap-2 justify-center">
-              <Image className="w-7 h-7 rounded-full" source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTXI-MAzeQW70mDdzZeOeDKlZTRSi43mhgpArfLMfF9g&s" }} />
-              <Text className="font-itim text-lg">Contiuar com o Google</Text>
-            </Pressable>
 
           </View>
         </View>
