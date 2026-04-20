41dbb4ecff413b71bebd7428bd0faa2497b7c16f
williamdev20 <williamalvs45@gmail.com>
2026-04-20 11:12:31 -0300

    feat(map): adicionar ícone personalizado na TabBar e adicionar o botão 'Conquistas' na TabBar

--- src/app/user/map/(tabs)/_layout.tsx
+++ src/app/user/map/(tabs)/_layout.tsx
@@ -1,8 +1,9 @@
 import { Tabs } from "expo-router";
 import { useFonts } from "expo-font";
 import { useRouter } from "expo-router";
-import { IrishGrover_400Regular } from "@expo-google-fonts/irish-grover"
-import { Itim_400Regular } from "@expo-google-fonts/itim"
+import { IrishGrover_400Regular } from "@expo-google-fonts/irish-grover";
+import { Itim_400Regular } from "@expo-google-fonts/itim";
+import { Image } from "react-native";
 import "@/global.css";
 
 export default function TabLayout() {
@@ -17,10 +18,46 @@ export default function TabLayout() {
 
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: "black", headerShown: false }}>
-        <Tabs.Screen name="index" options={{ title: "Explorar" }} />
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Explorar",
+            tabBarIcon: ({ focused }: { focused: boolean }) => (
+              <Image
+                source={require("@/assets/user/map/map-icon.png")}
+                className="w-8 h-8"
+                style={{ resizeMode: "contain", opacity: focused ? 1 : 0.5 }}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name="achievements"
+          options={{
+            title: "Conquistas",
+            tabBarIcon: ({ focused }: { focused: boolean }) => (
+              <Image
+                source={require("@/assets/user/map/achievements.png")}
+                className="w-8 h-8"
+                style={{ resizeMode: "contain", opacity: focused ? 1 : 0.5 }}
+              />
+            )
+          }}
+        />
+
         <Tabs.Screen
           name="redirect"
-          options={{ title: "Configurações" }}
+          options={{
+            title: "Configurações",
+            tabBarIcon: ({ focused }: { focused: boolean }) => (
+              <Image
+                source={require("@/assets/user/map/menu.png")}
+                className="w-8 h-8"
+                style={{ resizeMode: "contain", opacity: focused ? 1 : 0.5 }}
+              />
+            )
+          }}
           listeners={{
             tabPress: (e) => {
               e.preventDefault();
