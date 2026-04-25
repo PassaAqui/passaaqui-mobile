cb59b23429858249632343f67c3f94f4f00a38e3
williamdev20 <williamalvs45@gmail.com>
2026-04-25 18:48:05 -0300

    feat(shop): adicionar Header com nome da loja e botão para voltar pro mapa

--- src/app/user/map/poi/shop/index.tsx
+++ src/app/user/map/poi/shop/index.tsx
@@ -1,4 +1,5 @@
-import { ScrollView, View, Text, Image } from "react-native";
+import { ScrollView, View, Text, Image, Pressable } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import * as NavigationBar from "expo-navigation-bar";
 import XpBar from "@/src/components/user/map/poi/shop/XpBar";
@@ -21,43 +22,51 @@ export default function Shop() {
   }, [])
 
   return (
-    <ScrollView>
+    <SafeAreaView>
       <StatusBar style="dark" />
+      <View className="flex-row items-center justify-center p-2">
+        <Pressable className="absolute left-7 active:opacity-35">
+          <Image source={require("@/assets/user/settings/back.png")} />
+        </Pressable>
+        <Text className="font-itim text-black text-3xl" adjustsFontSizeToFit>Loja de fulano</Text>
+      </View>
 
-      <View className="flex-1 justify-center items-center w-full gap-3">
-        <View className="bg-[#C4843A] w-full p-7 rounded-lg flex-row gap-3 items-center">
-          <Image className="w-16 h-16" source={require("@/assets/user/map/poi/shop/coin.png")}/>
-          <View>
-            <Text className="font-itim text-white text-lg" adjustsFontSizeToFit>SEU SALDO DE XP</Text>
-            <Text className="font-itim text-white text-lg" adjustsFontSizeToFit>1207 XP</Text>
+      <ScrollView>
+        <View className="flex-1 justify-center items-center w-full p-4 gap-6">
+          <View className="bg-[#C4843A] w-full p-7 rounded-lg flex-row gap-3 items-center">
+            <Image className="w-16 h-16" source={require("@/assets/user/map/poi/shop/coin.png")}/>
+            <View>
+              <Text className="font-itim text-white text-lg" adjustsFontSizeToFit>SEU SALDO DE XP</Text>
+              <Text className="font-itim text-white text-lg" adjustsFontSizeToFit>1207 XP</Text>
+            </View>
           </View>
-        </View>
 
-        <View className="gap-3 flex-row flex-wrap items-center justify-center p-3">
-          {products.map((product) => (
-            <View key={product.id} className="border-2 border-[#EAAA6A] w-[47%] rounded-lg overflow-hidden">
-              <Image className="w-full h-28 bg-gray-400" source={require("@/assets/user/map/tmp/no-image.png")} resizeMode="cover" />
-              <View className="p-5 gap-3">
-                <Text className="text-lg">{product.title}</Text>
-                <Text>{product.description}</Text>
+          <View className="gap-6 flex-row flex-wrap items-center justify-center">
+            {products.map((product) => (
+              <View key={product.id} className="border-2 border-[#EAAA6A] w-[47%] rounded-lg overflow-hidden">
+                <Image className="w-full h-28 bg-gray-400" source={require("@/assets/user/map/tmp/no-image.png")} resizeMode="cover" />
+                <View className="p-5 gap-3">
+                  <Text className="text-lg">{product.title}</Text>
+                  <Text>{product.description}</Text>
 
-                <View className="flex-row gap-1 items-center">
-                  <Image className="w-6 h-6" source={require("@/assets/user/map/poi/shop/coin.png")} />
-                  <Text>R$ {product.price}</Text>
-                </View>
+                  <View className="flex-row gap-1 items-center">
+                    <Image className="w-6 h-6" source={require("@/assets/user/map/poi/shop/coin.png")} />
+                    <Text>R$ {product.price}</Text>
+                  </View>
 
-                <View>
-                  <XpBar currentXp={currentXp} xpRequired={product.xpRequired} />
                   <View>
-                    <Text className="font-itim">{currentXp}/{product.xpRequired} XP</Text>
-                    {/*<Image />*/}
+                    <XpBar currentXp={currentXp} xpRequired={product.xpRequired} />
+                    <View>
+                      <Text className="font-itim">{currentXp}/{product.xpRequired} XP</Text>
+                      {/*<Image />*/}
+                    </View>
                   </View>
                 </View>
               </View>
-            </View>
-          ))}          
+            ))}          
+          </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
