e523692c3fc8265dfd713a2d0d997898b3f22541
williamdev20 <williamalvs45@gmail.com>
2026-05-03 22:11:41 -0300

    fix(index): mudar fonte e corrigir responsividade da tela da loja dos POIs

--- src/app/user/map/poi/shop/index.tsx
+++ src/app/user/map/poi/shop/index.tsx
@@ -1,5 +1,5 @@
 import { ScrollView, View, Text, Image, Pressable, useWindowDimensions } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import * as NavigationBar from "expo-navigation-bar";
 import XpBar from "@/src/components/user/map/poi/shop/XpBar";
@@ -11,6 +11,7 @@ import { router } from "expo-router";
 const currentXp = 500;
 
 export default function Shop() {
+  const insets = useSafeAreaInsets();
   const { width } = useWindowDimensions();
 
   const PADDING = 16;
@@ -25,7 +26,7 @@ export default function Shop() {
   }, [])
 
   return (
-    <SafeAreaView className="bg-white">
+    <SafeAreaView edges={["top"]} className="flex-1 bg-white">
       <StatusBar style="dark" />
       <View className="flex-row items-center justify-center p-2">
         <Pressable className="absolute left-7 active:opacity-35">
@@ -34,13 +35,13 @@ export default function Shop() {
         <Text className="font-itim text-black text-3xl" adjustsFontSizeToFit>Loja de fulano</Text>
       </View>
 
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ paddingBottom: insets.bottom + 16 }}>
         <View className="flex-1 w-full p-4 gap-6">
           <View className="bg-[#C4843A] w-full p-7 rounded-lg flex-row gap-3 items-center">
             <Image className="w-16 h-16" source={require("@/assets/user/map/poi/shop/coin.png")}/>
-            <View>
-              <Text className="font-itim text-white text-lg" adjustsFontSizeToFit>SEU SALDO DE XP</Text>
-              <Text className="font-itim text-white text-lg" adjustsFontSizeToFit>1207 XP</Text>
+            <View className="flex-1">
+              <Text className="font-interBold text-white text-base" adjustsFontSizeToFit>SEU SALDO DE XP</Text>
+              <Text className="font-interBold text-white text-2xl" adjustsFontSizeToFit>1207 XP</Text>
             </View>
           </View>
 
@@ -54,18 +55,18 @@ export default function Shop() {
               >
                 <Image className="w-full h-28 bg-gray-400" source={require("@/assets/user/map/tmp/no-image.png")} resizeMode="cover" />
                 <View className="p-5 gap-3">
-                  <Text className="text-lg">{product.title}</Text>
-                  <Text>{product.description}</Text>
+                  <Text className="font-interBold text-lg">{product.title}</Text>
+                  <Text className="font-inter">{product.description}</Text>
 
                   <View className="flex-row gap-1 items-center">
                     <Image className="w-6 h-6" source={require("@/assets/user/map/poi/shop/coin.png")} />
-                    <Text>R$ {product.price}</Text>
+                    <Text className="font-interBold">R$ {product.price}</Text>
                   </View>
 
                   <View>
                     <XpBar currentXp={currentXp} xpRequired={product.xpRequired} thickness={1} />
                     <View className="flex-row gap-2 items-center">
-                      <Text className="font-itim">{currentXp}/{product.xpRequired} XP</Text>
+                      <Text className="font-inter text-sm">{currentXp}/{product.xpRequired} XP</Text>
                       <CompleteRequiredXp currentXp={currentXp} requiredXp={product.xpRequired} showText={false} />
                     </View>
                   </View>
