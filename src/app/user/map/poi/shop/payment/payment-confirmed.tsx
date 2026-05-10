dc080a60782177370428af0bf04dc41766befd6c
williamdev20 <williamalvs45@gmail.com>
2026-05-10 18:37:56 -0300

    fix(pix-confirmed): fazer o botão de 'Voltar à loja' levar o usuário de volta para a tela de visualização do produto

--- src/app/user/map/poi/shop/payment/payment-confirmed.tsx
+++ src/app/user/map/poi/shop/payment/payment-confirmed.tsx
@@ -4,8 +4,17 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import * as NavigationBar from "expo-navigation-bar";
 import { useEffect } from "react";
+import { useRouter } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
+import { products } from "@/src/constants/user/map/poi/shop/products";
 
 export default function PaymentConfirmed() {
+  const { id, discount } = useLocalSearchParams<{ id: string, discount: string }>();
+  const product = products.find(p => p.id === Number(id));
+
+  if (!product) return null;
+
+  const router = useRouter();
   const insets = useSafeAreaInsets();
 
   useEffect(() => {
@@ -18,8 +27,8 @@ export default function PaymentConfirmed() {
 
       <ScrollView className="flex-1" contentContainerStyle={{ paddingBottom: insets.bottom + 16 }} showsVerticalScrollIndicator={false}>
         <View className="items-center justify-center p-6 gap-6">
-          <View className="flex-col items-center justify-center gap-3">
-            <View className="bg-green-200 p-5 rounded-full border-2 border-green-500 items-center justify-center">
+          <View className="flex-col items-center justify-center gap-4">
+            <View className="bg-[#E8F5E9] p-5 rounded-full border-[3px] border-[#A8D5B0] items-center justify-center">
               <Image className="w-12 h-12" source={require("@/assets/user/map/poi/shop/check.png")} />
             </View>
 
@@ -29,43 +38,45 @@ export default function PaymentConfirmed() {
             </View>
           </View>
 
-          <View className="w-full items-center justify-center gap-5">
+          <View className="w-full items-center justify-center gap-5 mt-4">
             <View className="w-full border border-gray-400 flex-col rounded-2xl px-4 py-4">
               <Text className="font-interBold" adjustsFontSizeToFit>RESUMO DO PEDIDO</Text>
               <View className="w-full flex-row items-center py-3 gap-3">
                 <Image className="w-20 h-20 rounded-lg" source={{ uri: "https://static.thenounproject.com/png/3674270-200.png" }} />
                 <View className="flex-col flex-1">
-                  <Text className="text-lg font-interBold" adjustsFontSizeToFit>title</Text>
-                  <Text className="opacity-75 font-inter" adjustsFontSizeToFit>location</Text>
+                  <Text className="text-lg font-interBold" adjustsFontSizeToFit>{product.title}</Text>
+                  <Text className="opacity-75 font-inter" adjustsFontSizeToFit>{product.location}</Text>
                 </View>
-                <Text className="font-interBold text-lg">R$ PREÇO</Text>
+                <Text className="font-interBold text-lg">R$ {product.price.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}</Text>
               </View>
 
               <View className="h-px w-full bg-gray-300 mb-4" />
 
-              <View className="flex-row justify-between">
-                <Text className="opacity-75 font-inter flex-1" adjustsFontSizeToFit>Subtotal</Text>
-                <Text className="font-inter" adjustsFontSizeToFit>R$ PREÇO</Text>
-              </View>
-
-              <View className="flex-row justify-between">
-                <View className="flex-row gap-1 items-center flex-1">
-                  <Image className="w-6 h-6" source={require("@/assets/user/map/poi/shop/coin.png")} />
-                  <Text className="text-[#A86830] font-interBold text-lg flex-1" adjustsFontSizeToFit>Desconto XP</Text>
+              <View className="gap-1">
+                <View className="flex-row justify-between">
+                  <Text className="opacity-75 font-inter flex-1" adjustsFontSizeToFit>Subtotal</Text>
+                  <Text className="font-inter" adjustsFontSizeToFit>R$ {product.price.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}</Text>
                 </View>
-                <Text className="font-interBold text-green-600" adjustsFontSizeToFit>- R$ PREÇO</Text>
-              </View>
 
-              <View className="flex-row justify-between">
-                <Text className="opacity-75 font-inter flex-1" adjustsFontSizeToFit>Taxa de serviço</Text>
-                <Text className="font-inter" adjustsFontSizeToFit>R$ 0,00</Text>
+                <View className="flex-row justify-between">
+                  <View className="flex-row gap-1 items-center flex-1">
+                    <Image className="w-6 h-6" source={require("@/assets/user/map/poi/shop/coin.png")} />
+                    <Text className="text-[#A86830] font-interBold text-base flex-1" adjustsFontSizeToFit>Desconto XP</Text>
+                  </View>
+                  <Text className="font-interBold text-green-600" adjustsFontSizeToFit>- R$ {parseFloat(discount).toLocaleString("pt-BR", { minimumFractionDigits: 2 })}</Text>
+                </View>
+
+                <View className="flex-row justify-between">
+                  <Text className="opacity-75 font-inter flex-1" adjustsFontSizeToFit>Taxa de serviço</Text>
+                  <Text className="font-inter" adjustsFontSizeToFit>R$ 0,00</Text>
+                </View>
               </View>
 
               <View className="h-px w-full bg-gray-300 my-3" />
 
               <View className="flex-row justify-between items-center">
                 <Text className="font-interBold text-lg">Total pago</Text>
-                <Text className="font-interBold text-xl">R$ PREÇO</Text>
+                <Text className="font-interBold text-xl">R$ {(product.price - Number(discount)).toLocaleString("pt-BR", { minimumFractionDigits: 2 })}</Text>
               </View>
             </View>
 
@@ -115,7 +126,13 @@ export default function PaymentConfirmed() {
               <Text className="text-white font-interBold text-lg text-center">Ver meus resgates</Text>
             </Pressable>
 
-            <Pressable className="bg-transparent p-4 items-center justify-center rounded-xl active:opacity-50 border border-gray-400">
+            <Pressable
+              onPress={() => router.push({
+                pathname: "/user/map/poi/shop/product",
+                params: { id: product.id, discount }
+              })}
+              className="bg-transparent p-4 items-center justify-center rounded-xl active:opacity-50 border border-gray-400"
+            >
               <Text className="text-black font-interBold text-lg text-center">Voltar à loja</Text>
             </Pressable>
           </View>
