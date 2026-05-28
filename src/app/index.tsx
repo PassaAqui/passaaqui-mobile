6640645f1a5ac644a2918da3bce19bc7e390a5a8
williamdev20 <williamalvs45@gmail.com>
2026-05-28 01:43:27 -0300

    refactor: mover o HomeScreen solto dentro de features/home para features/home/screens

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
-import HomeScreen from "@/src/features/home/HomeScreen";
+import HomeScreen from "@/src/features/home/screens/HomeScreen";
 import { Redirect } from "expo-router";
 
 export default function Index() {
   // Apenas em desenvolvimento para testar novas telas
-  return <Redirect href={"/user/map/(tabs)"} />
+  //return <Redirect href={"/user/map/(tabs)"} />
 
   return <HomeScreen />
 }
