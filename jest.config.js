add8cc0132cff79c225387a841a2195653a92c6a
williamdev20 <williamalvs45@gmail.com>
2026-08-08 14:25:08 -0300

    test: separa testes unitários dos testes de integração e adiciona novos scripts de testes no package.json

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,35 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: "jest-expo",
-  setupFiles: ["<rootDir>/jest.env.ts"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
-  },
-  testMatch: ["**/__tests__/**/*.(test|spec).(ts|tsx)"],
-  testPathIgnorePatterns: ["/node_modules/", "/.expo/", "/src/e2e/"],
-  transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@stomp/stompjs)",
+  projects: [
+    {
+      displayName: "unit",
+      preset: "jest-expo",
+      setupFiles: ["<rootDir>/jest.env.ts"],
+      setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+      moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/$1",
+      },
+      testMatch: ["<rootDir>/src/**/__tests__/unit/**/*.(test|spec).(ts|tsx)"],
+      testPathIgnorePatterns: ["/node_modules/", "/.expo/", "/src/e2e/"],
+      transformIgnorePatterns: [
+        "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@stomp/stompjs)",
+      ],
+    },
+    {
+      displayName: "integration",
+      preset: "jest-expo",
+      setupFiles: ["<rootDir>/jest.env.ts"],
+      setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+      moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/$1",
+      },
+      testMatch: [
+        "<rootDir>/src/**/__tests__/integration/**/*.(test|spec).(ts|tsx)",
+      ],
+      testPathIgnorePatterns: ["/node_modules/", "/.expo/", "/src/e2e/"],
+      transformIgnorePatterns: [
+        "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@stomp/stompjs)",
+      ],
+    },
   ],
 };
