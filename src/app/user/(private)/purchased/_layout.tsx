96d5e7101a0ff69720b0a3f91866ff55dbeff492
williamdev20 <williamalvs45@gmail.com>
2026-08-16 23:21:13 -0300

    style: altera a cor do NavigationBar no _layout.tsx de purchased

--- src/app/user/(private)/purchased/_layout.tsx
+++ src/app/user/(private)/purchased/_layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout() {
   });
 
   useEffect(() => {
-    NavigationBar.setButtonStyleAsync("light");
+    NavigationBar.setButtonStyleAsync("dark");
   }, []);
 
   if (!fontsLoaded) return null;
